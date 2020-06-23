fde8b0adbbe97f385613a78fbbd047bfaabd898a
Vladyslav <vlad30vv@gmail.com>
2020-06-23 19:04:45 +0300

    Task 3: Send video stream to backend via websocket

--- lib/ui/camera.dart
+++ lib/ui/camera.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
-
+import 'package:image/image.dart' as imglib;
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+import 'dart:convert';
 
 List<CameraDescription> cameras;
 
-Camera() async {
+Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
     cameras = await availableCameras();
   } on CameraException catch (e) {
     print('Error: $e.code\nError Message: $e.message');
   }
-  runApp(CameraApp());
+  runApp(new CameraApp());
 }
 
 class CameraApp extends StatefulWidget {
@@ -24,34 +27,85 @@ class _CameraAppState extends State<CameraApp> {
   final WebSocketChannel channel = WebSocketChannel.connect(Uri.parse(
       "ws://ec2-18-218-127-154.us-east-2.compute.amazonaws.com/frame"));
   CameraController controller;
-  bool isDetecting = false;
 
   @override
   void initState() {
     super.initState();
 
-    this.channel.stream.listen(onDone());
+    this.channel.stream.listen(onData, onError: onError, onDone: onDone());
 
-    controller = CameraController(cameras[1], ResolutionPreset.medium);
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
     controller.initialize().then((_) {
       if (!mounted) {
         return;
       }
-
       setState(() {});
+      controller.startImageStream((CameraImage image) {});
+
+      Future<Image> convertYUV420toImageColor(CameraImage image) async {
+        try {
+          final int width = image.width;
+          final int height = image.height;
+          final int uvRowStride = image.planes[1].bytesPerRow;
+          final int uvPixelStride = image.planes[1].bytesPerPixel;
+
+          // print("uvRowStride: " + uvRowStride.toString());
+          // print("uvPixelStride: " + uvPixelStride.toString());
+
+          // imgLib -> Image package from https://pub.dartlang.org/packages/image
+          var img = imglib.Image(width, height); // Create Image buffer
+
+          // Fill image buffer with plane[0] from YUV420_888
+          for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+              final int uvIndex = uvPixelStride * (x / 2).floor() +
+                  uvRowStride * (y / 2).floor();
+              final int index = y * width + x;
+
+              final yp = image.planes[0].bytes[index];
+              final up = image.planes[1].bytes[uvIndex];
+              final vp = image.planes[2].bytes[uvIndex];
+              // Calculate pixel color
+              int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
+              int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
+                  .round()
+                  .clamp(0, 255);
+              int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
+              // color: 0x FF  FF  FF  FF
+              //           A   B   G   R
+              img.data[index] = (0xFF << 24) | (b << 16) | (g << 8) | r;
+            }
+          }
+          imglib.PngEncoder pngEncoder =
+              new imglib.PngEncoder(level: 0, filter: 0);
+          List<int> png = pngEncoder.encodeImage(img);
+
+          final bytes = png;
+          String img64 = base64Encode(bytes);
+          channel.sink.add(img64);
+        } catch (e) {
+          print(">>>>>>>>>>>> ERROR:" + e.toString());
+        }
+        return null;
+      }
+
+      controller.startImageStream(convertYUV420toImageColor);
     });
   }
 
   onDone() {
     debugPrint("Socket is closed");
   }
+
   onError(err) {
-    debugPrint(err.message);
+    debugPrint(err.runtimeType.toString());
+    WebSocketChannelException ex = err;
+    debugPrint(ex.message);
   }
+
   onData(event) {
     debugPrint(event);
   }
-
 
   @override
   void dispose() {
