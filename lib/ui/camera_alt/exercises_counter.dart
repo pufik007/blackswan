b2549bad39fddda96eb93c958b063e04b7d4c172
Vladyslav V <v.vasyliev@quantal.solutions>
2020-08-28 14:54:57 +0300

    Update exercises_counter.dart

--- lib/ui/camera_alt/exercises_counter.dart
+++ lib/ui/camera_alt/exercises_counter.dart
@@ -1,8 +1,12 @@
+import 'dart:collection';
+import 'package:tensorfit/ui/camera_alt/pose_match.dart';
 import 'vp_tree_manager.dart';
 import 'bbox.dart';
 
 class ExercisesCounter {
   VpTreeManager vpTreeManager;
+  double thresholdDistance;
+  LinkedHashMap<String, int> counter;
 
   init(config, exercise) {
     //   var config;
@@ -20,55 +24,60 @@ class ExercisesCounter {
       List<dynamic> pose, List<dynamic> confidence, Bbox bbox) {
     var nearestImage = vpTreeManager.getNearest(pose, confidence, bbox);
     //   return {'category': nearestImage[0], 'score': nearestImage[1][0]};
+
+    return PoseMatch();
   }
 
-  // incrementPoseCounter(match, conter, counter, threshold_distance) {
-  //   if (match['score'] < threshold_distance) {
-  //     if (conter.length == 0) {
-  //       counter.add([match['categort'], 1]);
-  //     } else if (counter[-1][0] == match['category']) {
-  //       counter[-1][1] += 1;
-  //     } else {
-  //       counter.add([match['category'], 1]);
-  //     }
-  //   } else {
-  //     if (counter.length == 0) {
-  //       counter.add(['unknown', 1]);
-  //     }
-  //     if (counter[-1][0] == 'unknown') {
-  //       counter[-1][1] += 1;
-  //     } else {
-  //       counter.add(['unknown', 1]);
-  //     }
-  //   }
-  // }
+  incrementPoseCounter(PoseMatch) {
+    if (PoseMatch.score < thresholdDistance) {
+    } else {}
 
-  // countTotalReps(pose, counter, patern) {
-  //   var currentPatternIndex = 0;
-  //   var countSquat = 0;
+    //   if (match['score'] < threshold_distance) {
+    //     if (conter.length == 0) {
+    //       counter.add([match['categort'], 1]);
+    //     } else if (counter[-1][0] == match['category']) {
+    //       counter[-1][1] += 1;
+    //     } else {
+    //       counter.add([match['category'], 1]);
+    //     }
+    //   } else {
+    //     if (counter.length == 0) {
+    //       counter.add(['unknown', 1]);
+    //     }
+    //     if (counter[-1][0] == 'unknown') {
+    //       counter[-1][1] += 1;
+    //     } else {
+    //       counter.add(['unknown', 1]);
+    //     }
+    //   }
+  }
 
-  //   for (var i = 0; i < counter; i++) {
-  //     if (counter[i] >= threshold_count) {
-  //       if (pose == patern[currentPatternIndex]) {
-  //         //pass - что равноценну оператору заглушке, не понял в чём смысл.
-  //       } else if (currentPatternIndex + 1 < pattern.length &&
-  //           pose == pattern[currentPatternIndex + 1]) {
-  //         currentPatternIndex += 1;
-  //       } else {
-  //         currentPatternIndex = 0;
-  //       }
-  //       if (currentPatternIndex == pattern.length - 1) {
-  //         countSquat += 1;
-  //         currentPatternIndex = 0;
-  //       }
-  //     }
-  //     return countSquat;
-  //   }
-  // }
+  countTotalReps(pose, counter, patern) {
+    //   var currentPatternIndex = 0;
+    //   var countSquat = 0;
+
+    //   for (var i = 0; i < counter; i++) {
+    //     if (counter[i] >= threshold_count) {
+    //       if (pose == patern[currentPatternIndex]) {
+    //         //pass - что равноценну оператору заглушке, не понял в чём смысл.
+    //       } else if (currentPatternIndex + 1 < pattern.length &&
+    //           pose == pattern[currentPatternIndex + 1]) {
+    //         currentPatternIndex += 1;
+    //       } else {
+    //         currentPatternIndex = 0;
+    //       }
+    //       if (currentPatternIndex == pattern.length - 1) {
+    //         countSquat += 1;
+    //         currentPatternIndex = 0;
+    //       }
+    //     }
+    //     return countSquat;
+    //   }
+  }
 
   repsCounter(List<dynamic> pose, List<dynamic> confidence, Bbox bbox) {
     var match = findMostSimilarMatch(pose, confidence, bbox);
-
+    incrementPoseCounter(PoseMatch);
     // incrementPoseCount(match);
     // var reps = countTotalReps();
     // return reps();
