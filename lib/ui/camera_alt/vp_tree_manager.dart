702638a15e392ed9422346e4822345daa28e2d17
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-28 17:19:04 +0300

    Update vp_tree_manager.dart

--- lib/ui/camera_alt/vp_tree_manager.dart
+++ lib/ui/camera_alt/vp_tree_manager.dart
@@ -25,15 +25,51 @@ class VpTreeManager {
   }
 
   distance(PoseSpacePoint a, PoseSpacePoint b) {
-    List<dynamic> confidence;
-    var dist;
-    var pointA = (a[0] - a[0].min()) / (a[0].max() - a[0].min());
-    var pointB = (b[0] - b[0].min()) / (b[0].max() - b[0].min());
+    var aMinValues = List<double>.from([double.maxFinite, double.maxFinite]);
+    var aMaxValues = List<double>.from([-double.maxFinite, -double.maxFinite]);
+    a.pose.forEach((joint) {
+      aMinValues[0] = min(aMinValues[0], joint[0]);
+      aMinValues[1] = min(aMinValues[1], joint[1]);
+      aMinValues[0] = max(aMaxValues[0], joint[0]);
+      aMinValues[1] = max(aMaxValues[1], joint[1]);
+    });
+    var pointA = a.pose.map<List<double>>((joint) {
+      var point = List<double>.from(joint);
+      point[0] = (point[0] - aMinValues[0]) / (aMaxValues[0] - aMinValues[0]);
+      point[1] = (point[1] - aMinValues[1]) / (aMaxValues[1] - aMinValues[1]);
+      return point;
+    });
 
-    if (a[0].min([x, y]) && b[0].min([x, y]) == a[1][0] && b[1][0]) {
-      return confidence = a[0] + b[0];
+    var bMinValues = List<double>.from([double.maxFinite, double.maxFinite]);
+    var bMaxValues = List<double>.from([-double.maxFinite, -double.maxFinite]);
+    b.pose.forEach((joint) {
+      bMinValues[0] = min(bMinValues[0], joint[0]);
+      bMinValues[1] = min(bMinValues[1], joint[1]);
+      bMaxValues[0] = max(bMaxValues[0], joint[0]);
+      bMaxValues[1] = max(bMaxValues[1], joint[1]);
+    });
+    var pointB = b.pose.map<List<double>>((joint) {
+      var point = List<double>.from(joint);
+      point[0] = (point[0] - bMinValues[0]) / (bMaxValues[0] - bMinValues[0]);
+      point[1] = (point[1] - bMinValues[1]) / (bMaxValues[1] - bMinValues[1]);
+      return point;
+    });
+
+    var confidence = List<double>();
+    var confidenceSum = .0;
+    for (int i = 0; i < a.pose.length; i++) {
+      var minConfidence = min(a.confidence[i], b.confidence[i]);
+      confidence.add(minConfidence);
+      confidenceSum += minConfidence;
     }
-    dist.sum((pointA + pointB * a[1][0] + b[1][0]) / confidence);
+    var dist = .0;
+    for (int i = 0; i < pointA.length; i++) {
+      dist += Vector.fromList(
+                  [pointA[i][0] - pointB[i][0], pointA[i][1] - pointB[i][1]])
+              .norm() *
+          confidence[i] /
+          confidenceSum;
+    }
 
     return dist;
   }
