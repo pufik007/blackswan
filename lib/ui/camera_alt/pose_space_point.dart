6b528f0eb2d2e6406e667b91345837aa13bc1425
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-28 17:50:58 +0300

    Update pose_space_point.dart

--- lib/ui/camera_alt/pose_space_point.dart
+++ lib/ui/camera_alt/pose_space_point.dart
@@ -2,7 +2,7 @@ import '../../vptree/space_point.dart';
 import '../camera_alt/bbox.dart';
 
 class PoseSpacePoint extends SpacePoint {
-  List<dynamic> pose;
+  List<List<double>> pose;
   List<dynamic> confidence;
   Bbox bbox;
 
