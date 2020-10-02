05cbf619666798d7fa96fac2e75ded6579790868
Vladyslav V <v.vasyliev@quantal.solutions>
2020-10-02 17:59:52 +0300

    add array of points "B"

--- test/ui/camera_alt/exercises_counter_test.dart
+++ test/ui/camera_alt/exercises_counter_test.dart
@@ -75,14 +75,15 @@ void main() {
       0.9729593
     ];
 
-    Bbox bboxFrame11 = {
-      // double minX = 0.4140942689298404,
-      // double maxX = 0.5580624861345514,
-      // double minY = 0.24532699099220184,
-      // double maxY = 0.8641534921542162,
-      // double B = 0.0841,
-      // double A = 0.094
-    };
+    Bbox bboxFrame11;
+    // = {
+    //   // double minX = 0.4140942689298404,
+    //   // double maxX = 0.5580624861345514,
+    //   // double minY = 0.24532699099220184,
+    //   // double maxY = 0.8641534921542162,
+    //   // double B = 0.0841,
+    //   // double A = 0.094
+    // };
 
     List<List<double>> poseFrame95 = [
       [0.47735545, 0.21991342],
@@ -116,14 +117,15 @@ void main() {
       0.9658314
     ];
 
-    Bbox bboxFrame95 = {
-      // double minX = 0.41224952244922364,
-      // double maxX = 0.5547129704306427,
-      // double minY = 0.21991342419249593,
-      // double maxY = 0.8590682357117567
-      // double B = 0.0869,
-      // double A = 0.0897
-    };
+    Bbox bboxFrame95;
+    // = {
+    //   // double minX = 0.41224952244922364,
+    //   // double maxX = 0.5547129704306427,
+    //   // double minY = 0.21991342419249593,
+    //   // double maxY = 0.8590682357117567
+    //   // double B = 0.0869,
+    //   // double A = 0.0897
+    // };
 
     List<List<double>> poseFrame170 = [
       [0.48525681, 0.13891301],
@@ -157,14 +159,15 @@ void main() {
       0.9789567
     ];
 
-    Bbox bboxFrame170 = {
-      // double minX = 0.41302757569239157,
-      // double maxX = 0.56006985084616,
-      // double minY = 0.1389130114641969,
-      // double maxY = 0.8530150114176054,
-      // double B =  0.1043,
-      // double A = 0.013
-    };
+    Bbox bboxFrame170;
+    // = {
+    //   // double minX = 0.41302757569239157,
+    //   // double maxX = 0.56006985084616,
+    //   // double minY = 0.1389130114641969,
+    //   // double maxY = 0.8530150114176054,
+    //   // double B =  0.1043,
+    //   // double A = 0.013
+    // };
 
     List<List<double>> poseFrame229 = [
       [0.4849827, 0.13518944],
@@ -198,14 +201,15 @@ void main() {
       0.9803678
     ];
 
-    Bbox bboxFrame229 = {
-      // double minX = 0.4161887450086017,
-      // double maxX = 0.553659208829746,
-      // double minY = 0.1351894437733983,
-      // double maxY = 0.8523295817498892,
-      // double B =  0.1153,
-      // double A = 0.0186
-    };
+    Bbox bboxFrame229;
+    // = {
+    //   // double minX = 0.4161887450086017,
+    //   // double maxX = 0.553659208829746,
+    //   // double minY = 0.1351894437733983,
+    //   // double maxY = 0.8523295817498892,
+    //   // double B =  0.1153,
+    //   // double A = 0.0186
+    // };
 
     List<List<double>> poseFrame315 = [
       [0.48331421, 0.13828378],
@@ -239,21 +243,232 @@ void main() {
       0.9740045
     ];
 
-    Bbox bboxFrame315 = {
-      // double minX = 0.4136966750474474,
-      // double maxX = 0.5527982402144186,
-      // double minY = 0.1382837775738056,
-      // double maxY = 0.8536017378131122,
-      // double B =  0.1139,
-      // double A = 0.0155
-    };
+    Bbox bboxFrame315;
+    // = {
+    //   // double minX = 0.4136966750474474,
+    //   // double maxX = 0.5527982402144186,
+    //   // double minY = 0.1382837775738056,
+    //   // double maxY = 0.8536017378131122,
+    //   // double B =  0.1139,
+    //   // double A = 0.0155
+    // };
+
+    List<List<double>> poseFrame60 = [
+      [0.48637921, 0.14018523],
+      [0.52929849, 0.23740194],
+      [0.43736516, 0.23923799],
+      [0.55530326, 0.36467979],
+      [0.4212977, 0.36569696],
+      [0.55557462, 0.47298467],
+      [0.41445126, 0.48134233],
+      [0.51971995, 0.4868716],
+      [0.45904935, 0.48604426],
+      [0.53289179, 0.67773646],
+      [0.44609149, 0.67674144],
+      [0.54962628, 0.85452139],
+      [0.43296544, 0.84936727],
+    ];
+
+    List<double> confidenceFrame60 = [
+      0.9742645,
+      0.94014895,
+      0.96332496,
+      0.9872289,
+      0.9910147,
+      0.9807511,
+      0.99348044,
+      0.7137668,
+      0.8746903,
+      0.90523875,
+      0.77232265,
+      0.95559514,
+      0.9730654
+    ];
+
+    Bbox bboxFrame60;
+    // = {
+    //   // double minX = 0.4144512572298687,
+    //   // double maxX = 0.5555746228955267,
+    //   // double minY = 0.14018522751939136,
+    //   // double maxY = 0.8545213891231742,
+    //   // double B =  0.1178,
+    //   // double A = 0.0098
+    // };
+
+    List<List<double>> poseFrame141 = [
+      [0.48085226, 0.4130615],
+      [0.52890367, 0.4878177],
+      [0.44845411, 0.4935379],
+      [0.55543354, 0.61195257],
+      [0.43249505, 0.61774057],
+      [0.51246979, 0.55741946],
+      [0.4555626, 0.56799551],
+      [0.51849075, 0.68678309],
+      [0.45746271, 0.68152645],
+      [0.57420652, 0.69250102],
+      [0.40242521, 0.69245907],
+      [0.55015096, 0.8697413],
+      [0.43158339, 0.87152759],
+    ];
+
+    List<double> confidenceFrame141 = [
+      0.98357224,
+      0.97605664,
+      0.9436015,
+      0.6688868,
+      0.90635777,
+      0.51771814,
+      0.93548435,
+      0.83295417,
+      0.70753646,
+      0.980126,
+      0.9922057,
+      0.63896567,
+      0.97232246
+    ];
+
+    Bbox bboxFrame141;
+    // = {
+    //   // double minX = 0.4024252097433439,
+    //   // double maxX = 0.574206522832526,
+    //   // double minY = 0.41306150432492617,
+    //   // double maxY = 0.8715275873760708,
+    //   // double B =  0.0272,
+    //   // double A = 0.1539
+    // };
+
+    List<List<double>> poseFrame203 = [
+      [0.48470211, 0.40275327],
+      [0.52880732, 0.47167929],
+      [0.44969307, 0.47893598],
+      [0.54505174, 0.57472245],
+      [0.43282475, 0.60519288],
+      [0.51953653, 0.53945151],
+      [0.45264362, 0.5454135],
+      [0.52284617, 0.67646407],
+      [0.46128611, 0.67097897],
+      [0.57510465, 0.69411679],
+      [0.40543611, 0.69772613],
+      [0.55015992, 0.8700967],
+      [0.43282841, 0.86725163],
+    ];
+
+    List<double> confidenceFrame203 = [
+      0.96905243,
+      0.96731496,
+      0.913831,
+      0.58959377,
+      0.74599516,
+      0.50397813,
+      0.90607196,
+      0.8543019,
+      0.47949952,
+      0.9924979,
+      0.99600476,
+      0.96930283,
+      0.9694973
+    ];
+
+    Bbox bboxFrame203;
+    // = {
+    //   // double minX = 0.40543611493584136,
+    //   // double maxX = 0.5751046536881932,
+    //   // double minY = 0.4027532690415697,
+    //   // double maxY = 0.8700966988744343,
+    //   // double B =  0.0349,
+    //   // double 0.1443
+    // };
+
+    List<List<double>> poseFrame275 = [
+      [0.48584993, 0.41407883],
+      [0.53044263, 0.48736024],
+      [0.45057217, 0.49450045],
+      [0.55976816, 0.59039822],
+      [0.42436055, 0.57834284],
+      [0.50252032, 0.52179769],
+      [0.46064625, 0.52771818],
+      [0.51711879, 0.68930564],
+      [0.457467, 0.66391571],
+      [0.57835189, 0.69745629],
+      [0.40699007, 0.69742578],
+      [0.5494047, 0.86434395],
+      [0.42865584, 0.85921114],
+    ];
+
+    List<double> confidenceFrame275 = [
+      0.9431748,
+      0.9553233,
+      0.94283825,
+      0.7937627,
+      0.89439243,
+      0.40633988,
+      0.52315766,
+      0.41432583,
+      0.7794298,
+      0.9733345,
+      0.99401635,
+      0.6120122,
+      0.93863595
+    ];
+
+    Bbox bboxFrame275;
+    // = {
+    //   // double minX = 0.4069900666981919,
+    //   // double maxX = 0.5783518859804982,
+    //   // double minY = 0.4140788272002101,
+    //   // double maxY = 0.8643439496348373,
+    //   // double B =  0.0331,
+    //   // double A = 0.1353
+    // };
+
+    List<List<double>> poseFrame345 = [
+      [0.47990693, 0.29491205],
+      [0.52772649, 0.38901567],
+      [0.44364507, 0.40340022],
+      [0.54591862, 0.48587502],
+      [0.42527147, 0.50195173],
+      [0.52395735, 0.47475867],
+      [0.44503347, 0.48056429],
+      [0.51905429, 0.58690336],
+      [0.45554231, 0.5946994],
+      [0.5673097, 0.68424965],
+      [0.40869111, 0.6842224],
+      [0.55106815, 0.8623355],
+      [0.42767288, 0.86281865],
+    ];
+
+    List<double> confidenceFrame345 = [
+      0.6284384,
+      0.7448628,
+      0.9017513,
+      0.76720333,
+      0.83734244,
+      0.49253622,
+      0.744395,
+      0.8391936,
+      0.85334694,
+      0.9803343,
+      0.98405254,
+      0.8958212,
+      0.9712334
+    ];
+
+    Bbox bboxFrame345;
+    // = {
+    //   // double minX = 0.40869110773808437,
+    //   // double maxX = 0.5673097045042728,
+    //   // double minY = 0.2949120450118515,
+    //   // double maxY = 0.8628186546302868,
+    //   // double B =  0.0454,
+    //   // double A = 0.1079
+    // };
 
     var poseSpacePointA1 = PoseSpacePoint(pose, confidence, bbox);
     var poseSpacePointA2 =
         PoseSpacePoint(poseFrame11, confidenceFrame11, bboxFrame11);
-    var poseSpacePointA4 =
-        PoseSpacePoint(poseFrame95, confidenceFrame95, bboxFrame95);
     var poseSpacePointA3 =
+        PoseSpacePoint(poseFrame95, confidenceFrame95, bboxFrame95);
+    var poseSpacePointA4 =
         PoseSpacePoint(poseFrame170, confidenceFrame170, bboxFrame170);
     var poseSpacePointA5 =
         PoseSpacePoint(poseFrame229, confidenceFrame229, bboxFrame229);
@@ -262,15 +477,15 @@ void main() {
 
     var poseSpacePointB1 = PoseSpacePoint(pose, confidence, bbox);
     var poseSpacePointB2 =
-        PoseSpacePoint(poseFrame11, confidenceFrame11, bboxFrame11);
-    var poseSpacePointB5 =
-        PoseSpacePoint(poseFrame95, confidenceFrame95, bboxFrame95);
+        PoseSpacePoint(poseFrame60, confidenceFrame60, bboxFrame60);
     var poseSpacePointB3 =
-        PoseSpacePoint(poseFrame170, confidenceFrame170, bboxFrame170);
+        PoseSpacePoint(poseFrame141, confidenceFrame141, bboxFrame141);
     var poseSpacePointB4 =
-        PoseSpacePoint(poseFrame229, confidenceFrame229, bboxFrame229);
+        PoseSpacePoint(poseFrame203, confidenceFrame203, bboxFrame203);
+    var poseSpacePointB5 =
+        PoseSpacePoint(poseFrame275, confidenceFrame275, bboxFrame275);
     var poseSpacePointB6 =
-        PoseSpacePoint(poseFrame315, confidenceFrame315, bboxFrame315);
+        PoseSpacePoint(poseFrame345, confidenceFrame345, bboxFrame345);
 
     var vpTreeA = new VpTreeFactory().build([
       poseSpacePointA1 as SpacePoint,
