e0bb8c0c477c1719b6c0f529ddfbface0bf39a75
Vladyslav V <v.vasyliev@quantal.solutions>
2020-10-16 19:00:47 +0300

    add lib to project.

--- lib/ui/camera_alt/camera_prediction_page.dart
+++ lib/ui/camera_alt/camera_prediction_page.dart
@@ -16,6 +16,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'vp_tree_manager.dart';
 import 'exercises_counter.dart';
 import '../../vptree/vptree.dart';
+import 'pose_space_point.dart';
+import 'bbox.dart';
+import 'pose_joint_lib.dart';
+import '../../vptree/vptree_factory.dart';
+import '../../vptree/space_point.dart';
 
 class CameraPredictionPage extends StatefulWidget {
   final List<CameraDescription> cameras;
@@ -37,30 +42,72 @@ class _CameraPredictionPageState extends State<CameraPredictionPage> {
   int _currentExerciseNo = 0;
   var namesExercise;
 
+
   ExerciseInfo exerciseInfo;
-  var vpTreesPool = Map<String, VpTree>();
   var vpTreeManager = VpTreeManager();
   var thresholdDistance = 0.1;
   var thresholdCount = 5;
   String exerciseKey = "E1";
   List<String> pattern = ["A", "B", "A"];
   ExercisesCounter repsCounter;
+  Bbox bbox;
+  var repsCount;
 
-  void _validExercises(List<dynamic> recognitions){
-    if(exerciseInfo.exercise.name == "Приседания"){
-      vpTreeManager.put(exerciseKey, vpTreesPool);
-      var counter = ExercisesCounter(vpTreeManager, 
+  List<PoseJointLib> extractPoseSpacePoints(List<dynamic> _recognitions){
+    var poseJointLib = List<PoseJointLib>();
+    List<List<double>> pose = List<List<double>>();
+    List<double> confidence = List<double>();
+    if (_recognitions.length > 0) {
+      var keypoints = _recognitions[0]["keypoints"];
+      if (keypoints != null) {
+        var keypointsList = keypoints.values.toList();
+        keypointsList.removeAt(1);
+        keypointsList.removeAt(1);
+        keypointsList.removeAt(1);
+        keypointsList.removeAt(1);
+        keypointsList.forEach((keypoint) {
+          poseJointLib.add(PoseJointLib(keypoint["x"], keypoint["y"], keypoint["score"])
+           );
+        });
+        poseJointLib.forEach((element) { 
+          pose.add([element.x, element.y]);
+          confidence.add(element.score);
+          var poseSpacePointA1 =
+          PoseSpacePoint(pose, confidence, bbox);
+              var poseSpacePointB1 =
+          PoseSpacePoint(pose, confidence, bbox);
+          
+          var vpTreeA = new VpTreeFactory().build([
+            poseSpacePointA1 as SpacePoint,
+          ], 6, (spacePointA, spacePointB) {
+            return VpTreeManager.distance(
+                spacePointA as PoseSpacePoint, spacePointB as PoseSpacePoint);
+          });
+
+          var vpTreeB = new VpTreeFactory().build([
+            poseSpacePointB1 as SpacePoint,
+  
+          ], 6, (spacePointA, spacePointB) {
+            return VpTreeManager.distance(
+                spacePointA as PoseSpacePoint, spacePointB as PoseSpacePoint);
+          });
+
+          var vpTreesPool = Map<String, VpTree>();
+          vpTreesPool['A'] = vpTreeA;
+          vpTreesPool['B'] = vpTreeB;
+
+          vpTreeManager.put(exerciseKey, vpTreesPool);
+          var counter = ExercisesCounter(vpTreeManager, 
                    exerciseKey,  
                    thresholdDistance, 
                    thresholdCount,
                    pattern);
-      var pose =  recognitions[0]["keypoints"];
-      var confidence;
-      var bbox;
-      counter.repsCounter(pose, confidence, bbox);
-      
-    }
+          repsCount = counter.repsCounter(pose, confidence, bbox);
+        });
+      }
 
+    }
+  return repsCount;
   }
 
   _startTimerCountdown() {
@@ -215,19 +262,11 @@ class _CameraPredictionPageState extends State<CameraPredictionPage> {
       } else {
         Navigator.pop(context);
       }
-      if(namesExercise == "Отжимания с колен"){
-         vpTreeManager.put(exerciseKey, vpTreesPool);
-      var counter = ExercisesCounter(vpTreeManager, 
-                   exerciseKey,  
-                   thresholdDistance, 
-                   thresholdCount,
-                   pattern);
-      var pose =  _recognitions[0]["keypoints"];
-      var confidence;
-      var bbox;
-      counter.repsCounter(pose, confidence, bbox);
+      if (namesExercise == "Отжимания с колен") {
+        extractPoseSpacePoints(_recognitions);
+        
       }
-    }
+    }  
     return Center(
       child: Container(
         padding: EdgeInsets.all(25),
@@ -274,6 +313,14 @@ class _CameraPredictionPageState extends State<CameraPredictionPage> {
                     math.min(_imageHeight, _imageWidth),
                     screen.height,
                     screen.width,
+                  ),
+                  Center(
+                    child: Text('$repsCount',
+                    style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                    fontSize: 20,
+                                  )),
                   ),
                   Padding(
                     padding: EdgeInsets.all(25),
---
+++ lib/ui/camera_alt/pose_joint_lib.dart
@@ -0,0 +1,11 @@
+class PoseJointLib {
+  double x;
+  double y;
+  double score;
+
+  PoseJointLib(double x, double y, double score) {
+    this.x = x;
+    this.y = y;
+    this.score = score;
+  }
+}
