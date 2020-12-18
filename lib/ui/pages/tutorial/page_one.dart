d6773f6f9af3ae4a866aac3dd9f9948f07269221
Vladyslav V <v.vasyliev@quantal.solutions>
2020-12-18 12:44:46 +0200

    add other variant of page Video

--- lib/ui/pages/tutorial/page_one.dart
+++ lib/ui/pages/tutorial/page_one.dart
@@ -62,9 +62,10 @@ class _PageOneState extends State<PageOne> {
                         padding: EdgeInsets.only(
                             bottom: 21, top: 21),
                         onPressed: () {
-                          Navigator.of(context)
-                            .push(MaterialPageRoute(builder: (context) =>
-                              VideoPage()
+                          Navigator.push<_PlayerVideoAndPopPage>(
+                            context,
+                            MaterialPageRoute<_PlayerVideoAndPopPage>(
+                              builder: (BuildContext context) => _PlayerVideoAndPopPage(),
                             ),
                           );
                         },
@@ -137,5 +138,63 @@ class _PageOneState extends State<PageOne> {
   void dispose() {
     super.dispose();
     _controller.dispose();
+  }
+}
+
+class _PlayerVideoAndPopPage extends StatefulWidget {
+  @override
+  _PlayerVideoAndPopPageState createState() => _PlayerVideoAndPopPageState();
+}
+
+class _PlayerVideoAndPopPageState extends State<_PlayerVideoAndPopPage> {
+  VideoPlayerController _videoPlayerController;
+  bool startedPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _videoPlayerController =
+        VideoPlayerController.network('https://tensorfit.com/video/demo.mp4');
+    _videoPlayerController.addListener(() {
+      if (startedPlaying && !_videoPlayerController.value.isPlaying) {
+        Navigator.pop(context);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  Future<bool> started() async {
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.play();
+    startedPlaying = true;
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 0,
+      child: Center(
+        child: FutureBuilder<bool>(
+          future: started(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (snapshot.data == true) {
+              return AspectRatio(
+                aspectRatio: _videoPlayerController.value.aspectRatio,
+                child: VideoPlayer(_videoPlayerController),
+              );
+            } else {
+              return const Text('waiting for video to load');
+            }
+          },
+        ),
+      ),
+    );
   }
 }
