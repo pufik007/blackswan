b259233b9054ba84800f3072860849c762ff071a
Vladyslav V <v.vasyliev@quantal.solutions>
2021-01-11 21:28:30 +0200

    Update level_state.dart

--- lib/ui/pages/level_bloc/level_state.dart
+++ lib/ui/pages/level_bloc/level_state.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:tensorfit/data/api/entities/exercise_info.dart';
 import 'package:tensorfit/data/api/entities/level.dart';
+import '../../../data/api/entities/exerciseDetection.dart';
 
 @immutable
 abstract class LevelState extends Equatable {
@@ -24,4 +25,13 @@ class LevelLoaded extends LevelState {
 
   @override
   List<Object> get props => [this.level, this.exercises];
+}
+
+class ExerciseDetectionLoaded extends LevelState {
+  final List<ExerciseDetection> exerciseDetections;
+
+  ExerciseDetectionLoaded(this.exerciseDetections);
+
+  @override
+  List<Object> get props => [this.exerciseDetections];
 }
