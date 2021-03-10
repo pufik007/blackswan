23eeef5ba002811d2dbe25bef72de01f1964f7bf
Vladyslav V <v.vasyliev@quantal.solutions>
2021-03-10 15:57:43 +0200

    camera detect add to front camera

--- lib/ui/camera_alt/camera.dart
+++ lib/ui/camera_alt/camera.dart
@@ -27,10 +27,8 @@ class _CameraState extends State<Camera> {
     if (widget.cameras == null || widget.cameras.length < 1) {
       print('No camera is found');
     } else {
-      controller = new CameraController(
-        widget.cameras[0],
-        ResolutionPreset.medium,
-      );
+      controller =
+          new CameraController(widget.cameras[1], ResolutionPreset.high);
       controller.initialize().then((_) {
         if (!mounted) {
           return;
@@ -125,19 +123,37 @@ class _CameraState extends State<Camera> {
     if (controller != null) {
       await controller.dispose();
     }
-    controller = CameraController(cameraDescription, ResolutionPreset.medium);
-    controller.addListener(() {
-      if (mounted) setState(() {});
-    });
-
-    try {
-      await controller.initialize();
-    } on CameraException catch (e) {
-      print(e);
-    }
-
-    if (mounted) {
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
       setState(() {});
-    }
+      controller.startImageStream((CameraImage img) {
+        if (!isDetecting) {
+          isDetecting = true;
+
+          int startTime = new DateTime.now().millisecondsSinceEpoch;
+          print('fps - $fpsThreshold');
+          if (startTime > fpsThreshold) {
+            Tflite.runPoseNetOnFrame(
+              bytesList: img.planes.map((plane) {
+                return plane.bytes;
+              }).toList(),
+              imageHeight: img.height,
+              imageWidth: img.width,
+              numResults: 2,
+            ).then((recognitions) {
+              int endTime = new DateTime.now().millisecondsSinceEpoch;
+              print("Detection took ${endTime - startTime}");
+
+              widget.setRecognitions(recognitions, img.height, img.width);
+
+              isDetecting = false;
+            });
+          }
+        }
+      });
+    });
   }
 }
