7fdd24c20387a2abe2c67f1e18fb581a6c1fd3d0
Vladyslav V <v.vasyliev@quantal.solutions>
2021-04-20 17:44:17 +0300

    add list with quotes and add text-quote to cart in home page

---
+++ lib/ui/pages/motivational_quotes.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/foundation.dart';
+
+class MotivationalQuotes {
+  final String id;
+  final String quote;
+  final String source;
+
+  MotivationalQuotes({
+    @required this.id,
+    @required this.quote,
+    @required this.source,
+  });
+}
+
+final List<MotivationalQuotes> quotesList = [
+  MotivationalQuotes(
+    id: '1',
+    quote: 'Don`t wish it were easier. Wish you were better.',
+    source: '― Jim Rohn',
+  ),
+  MotivationalQuotes(
+    id: '2',
+    quote:
+        'Don`t be pushed around by the fears in your mind. Be led by the dreams in your heart.',
+    source: '― Roy T. Bennett, The Light in the Heart',
+  ),
+  MotivationalQuotes(
+    id: '3',
+    quote:
+        'It’s only after you’ve stepped outside your comfort zone that you begin to change, grow, and transform.',
+    source: '― Roy T. Bennett',
+  ),
+  MotivationalQuotes(
+    id: '4',
+    quote: 'The man who moves a mountain begins by carrying away small stones.',
+    source: '― Confucius, Confucius: The Analects',
+  ),
+  MotivationalQuotes(
+    id: '5',
+    quote:
+        'The only way of discovering the limits of the possible is to venture a little way past them into the impossible.',
+    source: '― Arthur C. Clarke',
+  ),
+  MotivationalQuotes(
+    id: '6',
+    quote:
+        'Without ambition one starts nothing. Without work one finishes nothing. The prize will not be sent to you. You have to win it.',
+    source: '― Ralph Waldo Emerson',
+  ),
+  MotivationalQuotes(
+    id: '7',
+    quote: 'Learn the rules like a pro, so you can break them like an artist.',
+    source: '- Pablo Picasso',
+  ),
+  MotivationalQuotes(
+    id: '8',
+    quote:
+        'Action may not always bring happiness, but there is no happiness without action. ',
+    source: '― William James',
+  ),
+  MotivationalQuotes(
+    id: '9',
+    quote:
+        'I write for the same reason I breathe - because if I didn`t, I would die.',
+    source: '― Isaac Asimov',
+  ),
+  MotivationalQuotes(
+    id: '10',
+    quote:
+        'Only he who attempts the absurd is capable of achieving the impossible.',
+    source: '― Miguel de Unamuno',
+  ),
+  MotivationalQuotes(
+    id: '1',
+    quote: 'Don`t wish it were easier. Wish you were better.',
+    source: '― Jim Rohn',
+  ),
+  MotivationalQuotes(
+    id: '2',
+    quote:
+        'Don`t be pushed around by the fears in your mind. Be led by the dreams in your heart.',
+    source: '― Roy T. Bennett, The Light in the Heart',
+  ),
+  MotivationalQuotes(
+    id: '3',
+    quote:
+        'It’s only after you’ve stepped outside your comfort zone that you begin to change, grow, and transform.',
+    source: '― Roy T. Bennett',
+  ),
+  MotivationalQuotes(
+    id: '4',
+    quote: 'The man who moves a mountain begins by carrying away small stones.',
+    source: '― Confucius, Confucius: The Analects',
+  ),
+  MotivationalQuotes(
+    id: '5',
+    quote:
+        'The only way of discovering the limits of the possible is to venture a little way past them into the impossible.',
+    source: '― Arthur C. Clarke',
+  ),
+  MotivationalQuotes(
+    id: '6',
+    quote:
+        'Without ambition one starts nothing. Without work one finishes nothing. The prize will not be sent to you. You have to win it.',
+    source: '― Ralph Waldo Emerson',
+  ),
+  MotivationalQuotes(
+    id: '7',
+    quote: 'Learn the rules like a pro, so you can break them like an artist.',
+    source: '- Pablo Picasso',
+  ),
+  MotivationalQuotes(
+    id: '8',
+    quote:
+        'Action may not always bring happiness, but there is no happiness without action. ',
+    source: '― William James',
+  ),
+  MotivationalQuotes(
+    id: '9',
+    quote:
+        'I write for the same reason I breathe - because if I didn`t, I would die.',
+    source: '― Isaac Asimov',
+  ),
+  MotivationalQuotes(
+    id: '10',
+    quote:
+        'Only he who attempts the absurd is capable of achieving the impossible.',
+    source: '― Miguel de Unamuno',
+  ),
+  MotivationalQuotes(
+    id: '1',
+    quote: 'Don`t wish it were easier. Wish you were better.',
+    source: '― Jim Rohn',
+  ),
+  MotivationalQuotes(
+    id: '2',
+    quote:
+        'Don`t be pushed around by the fears in your mind. Be led by the dreams in your heart.',
+    source: '― Roy T. Bennett, The Light in the Heart',
+  ),
+  MotivationalQuotes(
+    id: '3',
+    quote:
+        'It’s only after you’ve stepped outside your comfort zone that you begin to change, grow, and transform.',
+    source: '― Roy T. Bennett',
+  ),
+  MotivationalQuotes(
+    id: '4',
+    quote: 'The man who moves a mountain begins by carrying away small stones.',
+    source: '― Confucius, Confucius: The Analects',
+  ),
+  MotivationalQuotes(
+    id: '5',
+    quote:
+        'The only way of discovering the limits of the possible is to venture a little way past them into the impossible.',
+    source: '― Arthur C. Clarke',
+  ),
+  MotivationalQuotes(
+    id: '6',
+    quote:
+        'Without ambition one starts nothing. Without work one finishes nothing. The prize will not be sent to you. You have to win it.',
+    source: '― Ralph Waldo Emerson',
+  ),
+  MotivationalQuotes(
+    id: '7',
+    quote: 'Learn the rules like a pro, so you can break them like an artist.',
+    source: '- Pablo Picasso',
+  ),
+  MotivationalQuotes(
+    id: '8',
+    quote:
+        'Action may not always bring happiness, but there is no happiness without action. ',
+    source: '― William James',
+  ),
+  MotivationalQuotes(
+    id: '9',
+    quote:
+        'I write for the same reason I breathe - because if I didn`t, I would die.',
+    source: '― Isaac Asimov',
+  ),
+  MotivationalQuotes(
+    id: '10',
+    quote:
+        'Only he who attempts the absurd is capable of achieving the impossible.',
+    source: '― Miguel de Unamuno',
+  ),
+];
